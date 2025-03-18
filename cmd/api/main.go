ebc53521187a6f0e49f22ce77e85a3ea03ee3301
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-03-18 22:11:51 +0900

    adding a logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/kaung-san-hein/go-social/internal/db"
 	"github.com/kaung-san-hein/go-social/internal/env"
 	"github.com/kaung-san-hein/go-social/internal/store"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.1"
@@ -39,6 +38,11 @@ func main() {
 		env: env.GetString("ENV", "development"),
 	}
 
+	// Logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// Database
 	db, err := db.New(
 		cfg.db.addr,
 		cfg.db.maxOpenConns,
@@ -46,19 +50,20 @@ func main() {
 		cfg.db.maxIdleTime,
 	)
 	if err != nil {
-		log.Panic(err)
+		logger.Fatal(err)
 	}
 
 	defer db.Close()
-	log.Println("database connection pool established")
+	logger.Info("database connection pool established")
 
 	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
 		store:  store,
+		logger: logger,
 	}
 
 	mux := app.mount()
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 }
