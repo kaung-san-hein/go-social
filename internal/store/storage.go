64ce55a6eeb2f9fc73a79238edca6c65d09e8246
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-03-19 10:19:12 +0900

    user registration

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -24,6 +24,7 @@ type Storage struct {
 	Users interface {
 		GetByID(context.Context, int64) (*User, error)
 		Create(context.Context, *User) error
+		CreateAndInvite(context.Context, *User, string) error
 	}
 	Comments interface {
 		GetByPostID(context.Context, int64) ([]Comment, error)
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,14 +3,33 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID        int64  `json:"id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Password  string `json:"-"`
-	CreatedAt string `json:"created_at"`
+	ID        int64    `json:"id"`
+	Username  string   `json:"username"`
+	Email     string   `json:"email"`
+	Password  password `json:"-"`
+	CreatedAt string   `json:"created_at"`
+}
+
+type password struct {
+	text *string
+	hash []byte
+}
+
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.text = &text
+	p.hash = hash
+
+	return nil
 }
 
 type UserStore struct {
@@ -77,4 +96,11 @@ func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error) {
 	}
 
 	return user, nil
+}
+
+func (s *UserStore) CreateAndInvite(ctx context.Context, user *User, token string) error {
+	// transaction wrapper
+	// create the user
+	// create the user invite
+	return nil
 }
