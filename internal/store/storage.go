455e6e30d2c7aadc80b9eb547fd6fec20ab027e6
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-03-19 12:46:41 +0900

    user activation

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -25,6 +25,7 @@ type Storage struct {
 		GetByID(context.Context, int64) (*User, error)
 		Create(context.Context, *sql.Tx, *User) error
 		CreateAndInvite(context.Context, *User, string, time.Duration) error
+		Activate(context.Context, string) error
 	}
 	Comments interface {
 		GetByPostID(context.Context, int64) ([]Comment, error)
